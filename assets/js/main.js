d0cc938cb3ff875a8e3e6b9a7abe07beb82745b9
Greg Remiasz <gremiasz@tribpub.com>
2019-02-13 16:25:40 -0600

    load tawk.to after page load event

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,15 +91,16 @@ $(function(){
           }
       };
 
-      (function() {
+      $(window).on('load', function() {
         var s1 = document.createElement("script"),
           s0 = document.getElementsByTagName("script")[0];
+        s1.defer = true;
         s1.async = true;
         s1.src = 'https://embed.tawk.to/57356416163d0cca65fcfc32/default';
         s1.charset = 'UTF-8';
         s1.setAttribute('crossorigin', '*');
         s0.parentNode.insertBefore(s1, s0);
-      })();
+      });
 
       // Tawk.to Chat End
 
